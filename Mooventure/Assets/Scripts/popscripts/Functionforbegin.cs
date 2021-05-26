f0fcdd9d5bd57282995e64b0630c5196f44aaf6d
raphel27 <50395735+raphel27@users.noreply.github.com>
2021-05-26 00:38:20 -0700

    Adding functions to bottoms, change few scenes and create pop up window

---
+++ Mooventure/Assets/Scripts/popscripts/Functionforbegin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functionforbegin : MonoBehaviour
+{
+
+    [SerializeField] private GameObject window;
+    [SerializeField] private GameObject parent;
+
+
+    // Start is called before the first frame update
+    public void create_pop_window()
+    {
+        Instantiate(window, parent.transform);
+    }
+    public void quitgame()
+    {
+        
+    }
+}
